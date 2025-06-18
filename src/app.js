014a3b875b463d861c0832abfb315fcf10b16802
Alejo Beliz <129092769+albelizGH@users.noreply.github.com>
2025-06-18 19:47:55 -0300

    Fix: cors

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import morgan from "morgan";
+import cors from "cors";
 
 
 import usuarioRoutes from "./routes/usuario.routes";
@@ -8,13 +9,13 @@ import productoRoutes from "./routes/producto.routes";
 
 const app = express();
 
-
 app.set("port", 4000);
-
 
 app.use(morgan("dev"));
 
 app.use(express.json());
+
+app.use(cors());
 
 //Routes
 app.use("/api",usuarioRoutes);
